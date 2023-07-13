cbb1748b436d025f7e72ba5014a27748dbdf911e
yummyweb <antriksh.timepass@gmail.com>
2023-07-14 00:11:03 +0530

    changed apostrophes

--- utils/client.js
+++ utils/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "<SUPABASE-URL-HERE>"
-const supabaseKey = "<SUPABASE-KEY-HERE>"
+const supabaseUrl = "https://mhecorawfcniksqvxtmb.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oZWNvcmF3ZmNuaWtzcXZ4dG1iIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NzY3NTUwNiwiZXhwIjoyMDAzMjUxNTA2fQ.eBb8A9qkmC7RtJks58PUhSGzUj0PYorJVoCg2n2QmMw"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
